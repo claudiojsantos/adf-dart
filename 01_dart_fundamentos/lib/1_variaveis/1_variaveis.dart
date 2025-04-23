9cc0dcfdf9333df097c04a081e87a65e3f8d00bb
Cláudio Santos <cljos@hotmail.com>
2025-04-22 21:17:01 -0300

    chapter 1: dart fundaments

---
+++ 01_dart_fundamentos/lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+
+void main(){
+  var idade = 50;
+  var valorIphone = 12000.00;
+  var nomeCurso = "Academia do FLutter";
+
+  Object objetoQualquer = 1.2;
+
+  dynamic dynamicQualquer = 1;
+
+}
---
+++ 01_dart_fundamentos/lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,12 @@
+void main(){
+  String nomeCompleto = "Cláudio Santos";
+  print(nomeCompleto);
+
+  // final
+  final nomeFinal = "Aldinho";
+  print(nomeFinal);
+
+  // Const
+  const nomeConst = "Aldo";
+  print(nomeConst);
+}
